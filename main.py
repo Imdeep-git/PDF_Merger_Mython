b553e09c3c6eef98059f16ad500f493841a66ab7
Deep Acharya <deepacharya374@gmail.com>
2023-06-05 12:04:02 +0545

    files commited

---
+++ main.py
@@ -0,0 +1,15 @@
+#pip install PyPDF2
+from PyPDF2 import PdfMerger
+pdfs = []
+num = int(input("enter the no of pdf files you want to merge\n"))
+for i in range(num):
+    x = input(f"Enter the {i+1}th pdf file path: \n ")
+    pdfs.append(x)
+
+merger = PdfMerger()
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("result.pdf")
+merger.close()
